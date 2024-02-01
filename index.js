2bf8489ffa47bfb0e9a1dfdfe3c91485f9af590f
skMisael <sk-mis@hotmail.com>
2024-01-31 23:22:25 -0600

    URL

--- index.js
+++ index.js
@@ -27,7 +27,9 @@ app.use('/api/auth', require('./routes/auth'));
 //middleware para eventos
 app.use('/api/events', require('./routes/events'));
 
-
+app.get('*',(req, res) =>{
+    res.sendFile(__dirname+'/public/index.html');
+});
 
 //escuchar peticiones
 app.listen(process.env.PORT, ()=>{
